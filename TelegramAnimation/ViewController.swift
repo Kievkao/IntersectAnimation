2f0cc9b653561e93a1d12f121e94e6451ac9de56
akravchenko <akravchenko@cogniance.com>
2016-07-16 19:15:02 +0300

    Initial Commit

---
+++ TelegramAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TelegramAnimation
+//
+//  Created by Andrii Kravchenko on 7/16/16.
+//  Copyright © 2016 kievkao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
